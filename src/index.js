a8b505838f3668f43c4a5b5d035ba97c966115d0
Luan <luan.andryl@gmail.com>
2019-02-17 08:17:21 -0300

    add: redux provider wrapper

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import Auth from './pages/Auth/Index'
 import Todo from './pages/Todo/Index'
 import * as serviceWorker from './serviceWorker';
+import store from './store/store'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={Auth} />
-            <Route path="/todo" exact={true} component={Todo} />
-        </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact={true} component={Auth} />
+                <Route path="/todo" exact={true} component={Todo} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
