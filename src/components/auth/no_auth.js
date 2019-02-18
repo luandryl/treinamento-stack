dde22cf7365a999799997876e0b145318face086
Luan <luan.andryl@gmail.com>
2019-02-18 04:15:56 -0300

    hoc auth routes

---
+++ src/components/auth/no_auth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export default function (ComposedComponent) {
+  class Authentication extends Component {
+    componentWillMount() {
+      if (this.props.authorized) {
+        this.props.history.push('/todo/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (nextProps.authorized) {
+        this.props.history.push('/todo/');
+      }
+    }
+
+    PropTypes = {
+      router: PropTypes.object,
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authorized: state.auth.authorized };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
