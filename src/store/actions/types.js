fab7b2d78f74990e9b108a0f82bf069b903618d8
Luan <luan.andryl@gmail.com>
2019-02-18 04:18:47 -0300

    types

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -2,3 +2,8 @@ export const FETCH_TODOS = 'FETCH_TODOS'
 export const NEW_TODO = 'NEW_TODO'
 export const COMPLETE_TASK = 'COMPLETE_TASK'
 export const REMOVE_TASK = 'REMOVE_TASK'
+
+export const authorized = 'authorized'
+export const unauthorized = 'unauthorized'
+export const auth_error = 'auth_error'
+
