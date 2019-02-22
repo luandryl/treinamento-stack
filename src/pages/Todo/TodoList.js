c61b2b166380493a75dcadbdd8d69b81f246bde1
Luan <luan.andryl@gmail.com>
2019-02-22 05:02:38 -0300

    fix: case of 0 tasks

--- src/pages/Todo/TodoList.js
+++ src/pages/Todo/TodoList.js
@@ -29,14 +29,18 @@ class TodoList extends Component {
   }
 
   render () {
-    let todoList = this.props.list.map((m, i) => {
-      let controlBtn = (m.status === true) ? (<i className="fas fa-trash" onClick={(e)=>{this.removeTask(e, m)}} ></i>): (<i className="fas fa-check" onClick={(e) => {this.completeTask(e, m)}}></i>)
-      return <li key={i} className={m.status ? 'completedTask':''}> {m.data} {controlBtn}</li>
-    })
-  
-    let todoBox = (this.props.isLoading !== true)?(<ul>{todoList}</ul>) : (<div className="loader"><i className="fas fa-spinner fa-5x"></i></div>)
+    if (this.props.list.length > 0) {
+      let todoList = this.props.list.map((m, i) => {
+        let controlBtn = (m.status === true) ? (<i className="fas fa-trash" onClick={(e)=>{this.removeTask(e, m)}} ></i>): (<i className="fas fa-check" onClick={(e) => {this.completeTask(e, m)}}></i>)
+        return <li key={i} className={m.status ? 'completedTask':''}> {m.data} {controlBtn}</li>
+      })
     
-    return todoBox
+      let todoBox = (this.props.isLoading !== true)?(<ul>{todoList}</ul>) : (<div className="loader"><i className="fas fa-spinner fa-5x"></i></div>)
+      
+      return todoBox
+    } else {
+      return <li>write some task and press add</li>
+    }
   }
 }
 TodoList.prototypes = {
